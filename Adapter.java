e0e75b769e9dea297f0f45d46d4cd0ab2f3c9716
FangPingYuan <58929840+FangPingYuan@users.noreply.github.com>
2021-09-01 00:27:44 +0800

    Create Adapter.java

---
+++ Adapter.java
@@ -0,0 +1,39 @@
+public class Adapter implements TrigonometricFunction{
+	
+	@Override
+	public void Sin() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Cos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Tan() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Csc() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Sec() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Cot() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
